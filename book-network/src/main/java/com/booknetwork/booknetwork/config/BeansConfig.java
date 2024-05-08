ab7960f02d132a711e9229a460fb7a214b3279b6
EDCarlitosxD <juanuchdzib@gmail.com>
2024-05-08 14:13:27 -0500

    BeansConfig.java DaoAuthenticationProvider and PasswordEncoder BCrypt

---
+++ book-network/src/main/java/com/booknetwork/booknetwork/config/BeansConfig.java
@@ -0,0 +1,35 @@
+package com.booknetwork.booknetwork.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeansConfig {
+
+    private final UserDetailsService userDetailsService;
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()throws Exception{
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
