a11f5b18ffbde2f202c8a501679905dbec49a008
EDCarlitosxD <juanuchdzib@gmail.com>
2024-05-13 09:12:36 -0500

    Book, BookTransactionHistory.java and Feedback.java. Relations entities

---
+++ book-network/src/main/java/com/booknetwork/booknetwork/book/Book.java
@@ -0,0 +1,56 @@
+package com.booknetwork.booknetwork.book;
+
+import com.booknetwork.booknetwork.common.BaseEntity;
+import com.booknetwork.booknetwork.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class Book extends BaseEntity {
+
+
+    private String title;
+    private String authorName;
+    private String isbn;
+    private String synopsis;
+    private String bookCover;
+    private boolean archived;
+    private boolean shareable;
+
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+
+    @OneToMany(mappedBy = "book")
+    private List<Feedback> feedbacks;
+
+
+    @OneToMany(mappedBy = "book")
+    private List<BookTransactionHistory> histories;
+
+
+
+    @Transient
+    public double getRate(){
+        if(feedbacks == null || feedbacks.isEmpty()){
+            return 0.0;
+        }
+
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+
+        return Math.round(rate * 10.0) / 10.0;
+    }
+}
---
+++ book-network/src/main/java/com/booknetwork/booknetwork/book/BookTransactionHistory.java
@@ -0,0 +1,30 @@
+package com.booknetwork.booknetwork.book;
+
+import com.booknetwork.booknetwork.common.BaseEntity;
+import com.booknetwork.booknetwork.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
